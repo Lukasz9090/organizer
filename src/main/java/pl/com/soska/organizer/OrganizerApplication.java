01f8f4517f31d7d8460041ff501f31ccdf2c3c27
LukaszS <lukasz.soskaa@gmail.com>
2019-09-22 23:22:54 +0200

    first step

---
+++ src/main/java/pl/com/soska/organizer/OrganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.com.soska.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizerApplication.class, args);
+    }
+
+}
