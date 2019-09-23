e1d1bc93df016a4752a150dc727e339852c76b69
LukaszS <lukasz.soskaa@gmail.com>
2019-09-23 13:29:07 +0200

    refaktor

--- src/main/java/pl/com/soska/organizer/model/User.java
+++ src/main/java/pl/com/soska/organizer/model/User.java
@@ -1,7 +1,6 @@
 package pl.com.soska.organizer.model;
 
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.Email;
