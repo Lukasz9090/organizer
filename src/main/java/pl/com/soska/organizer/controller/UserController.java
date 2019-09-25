8a6cf2b95d3d66ca3fb80fbb2f58a5d5e500957c
LukaszS <lukasz.soskaa@gmail.com>
2019-09-25 22:33:34 +0200

    create controllers

--- src/main/java/pl/com/soska/organizer/controller/UserController.java
+++ src/main/java/pl/com/soska/organizer/controller/UserController.java
@@ -1,6 +1,12 @@
 package pl.com.soska.organizer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.com.soska.organizer.model.User;
 import pl.com.soska.organizer.service.UserService;
 
 @Controller
@@ -12,5 +18,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/register")
+    public String register (Model model){
+        model.addAttribute("user", new User());
+        return "register-form";
+    }
 
+    @PostMapping("register")
+    public String addUser(@ModelAttribute User user,
+                          BindingResult bindResult){
+        if(bindResult.hasErrors()){
+            return "register-form";
+        }
+        else {
+            userService.createUser(user);
+            return "register-success";
+        }
+    }
 }
