90a606c3b9d8abaf68fe72d48fb5537f90bbcd4d
LukaszS <lukasz.soskaa@gmail.com>
2019-09-25 15:11:06 +0200

    add WebSecurityConfig

--- src/main/java/pl/com/soska/organizer/OrganizerApplication.java
+++ src/main/java/pl/com/soska/organizer/OrganizerApplication.java
@@ -3,7 +3,7 @@ package pl.com.soska.organizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // @Configuration, @EnableAutoConfiguration, @ComponentScan
 public class OrganizerApplication {
 
     public static void main(String[] args) {
