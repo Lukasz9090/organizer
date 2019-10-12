56afff4bf3626d5c903c5f53a940afe40d96422b
Lukasz9090 <49239484+Lukasz9090@users.noreply.github.com>
2019-10-12 11:33:28 +0200

    Revert "Feature confirm email addrees"

--- src/main/java/pl/com/organizer/exception/UnconfirmedAccountException.java
+++
@@ -1,7 +0,0 @@
-package pl.com.organizer.exception;
-
-public class UnconfirmedAccountException extends RuntimeException {
-    public UnconfirmedAccountException(String message) {
-        super(message);
-    }
-}
