aa22139c64c36acfbc760ba0b19944240eda7914
Lukasz9090 <lukasz.soskaa@gmail.com>
2019-10-22 14:31:41 +0200

    add Exception Handler to report generator

--- src/main/java/pl/com/organizer/controllermvc/ReportGeneratorController.java
+++ src/main/java/pl/com/organizer/controllermvc/ReportGeneratorController.java
@@ -6,10 +6,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import pl.com.organizer.exception.IncorrectDateException;
 import pl.com.organizer.model.ReportSettings;
 import pl.com.organizer.service.ReportGeneratorService;
 
@@ -40,5 +38,17 @@ public class ReportGeneratorController {
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.add("content-disposition", "inline; filename=" + "Expenses report.pdf");
         return new ResponseEntity<>(pdfContents, headers, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(IncorrectDateException.class)
+    public String exceptionHandlerForIncorrectDate (Model model){
+        model.addAttribute("message", "You have selected the wrong dates");
+        return "report-error-page";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String exceptionHandlerForNullDate (Model model){
+        model.addAttribute("message", "You have to select dates");
+        return "report-error-page";
     }
 }
